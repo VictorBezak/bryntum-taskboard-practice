d04f78ab5e0a8fbab493566476a225eb6bf9c495
Victor J. Bezak III <victorjbezak@gmail.com>
2024-12-05 12:13:31 -0600

    remove public assets not used & format app with 4-space indentation

--- src/TaskBoardConfig.ts
+++ src/TaskBoardConfig.ts
@@ -1,36 +1,35 @@
 import { BryntumTaskBoardProps } from '@bryntum/taskboard-react';
 
 const taskboardProps: BryntumTaskBoardProps = {
-   columns : [
-       { id : 'todo', text : 'Todo', color : 'orange' },
-       { id : 'doing', text : 'Doing', color : 'blue', tooltip : 'Items that are currently in progress' },
-       { id : 'done', text : 'Done' }
-   ],
-    tbar : {
-        items : [
+    columns: [
+        { id: 'todo', text: 'Todo', color: 'orange' },
+        { id: 'doing', text: 'Doing', color: 'blue', tooltip: 'Items that are currently in progress' },
+        { id: 'done', text: 'Done' }
+    ],
+    tbar: {
+        items: [
             {
-                type : 'button',
-                text : 'My button'
+                type: 'button',
+                text: 'My button'
             }
         ]
     },
 
-   // Field used to pair a task to a column
-   columnField : 'status',
+    // Field used to pair a task to a column
+    columnField: 'status',
 
-   project : {
-       transport : {
-           load : {
-               url : 'data.json'
-           }
-       },
-       autoLoad : true
-   },
-   taskDragFeature: true,
-   onTaskDrop: (event) => {
-    console.log("event", event)
-   }
-
+    project: {
+        transport: {
+            load: {
+                url: 'data.json'
+            }
+        },
+        autoLoad: true
+    },
+    taskDragFeature: true,
+    onTaskDrop: (event) => {
+        console.log("event", event)
+    }
 };
 
 export { taskboardProps };
